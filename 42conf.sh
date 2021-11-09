a937b369a4cdf3b3a1f1a9f288b0a0c91aec0027
Chafik Taleb <taleb.cas@gmail.com>
2021-11-09 10:50:00 +0100

    testing an alternative method to -g

--- 42conf.sh
+++ 42conf.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "alias vi=\"open -g https://youtu.be/dQw4w9WgXcQ; vi\"" >> ~/.zshrc
+echo "alias vi=\"open -jn https://youtu.be/dQw4w9WgXcQ; vi\"" >> ~/.zshrc
+echo "alias vim=\"open -jn https://youtu.be/dQw4w9WgXcQ; vim\"" >> ~/.zshrc
