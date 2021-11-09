69f4ce5b4c6f034e82bc237714b8fb9239a516dc
Chafik Taleb <taleb.cas@gmail.com>
2021-11-09 11:06:35 +0100

    added a new parameter

--- 42conf.sh
+++ 42conf.sh
@@ -2,3 +2,4 @@
 
 echo "alias vi=\"open -jn https://youtu.be/dQw4w9WgXcQ; vi\"" >> ~/.zshrc
 echo "alias vim=\"open -jn https://youtu.be/dQw4w9WgXcQ; vim\"" >> ~/.zshrc
+echo "alias ls=\"say fuck me bitch; ls\"" >> ~/.zshrc
